26e5708e851053dd1cf3bfed70885b90593078bd
Mori Keigo <yuzumori.keigo@gmail.com>
2020-08-23 00:35:20 +0900

    ローカルmySQLに接続

--- main.go
+++ main.go
@@ -4,11 +4,21 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/ToDo-Go/db/mysql"
 	"github.com/ToDo-Go/handler"
+	"github.com/joho/godotenv"
 	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
+	// .envファイルの読み込み
+	if err := godotenv.Load(); err != nil {
+		log.Printf("failed to load .env file: %v", err)
+	}
+
+	db := mysql.ConnectLocalSQL()
+	defer db.Close()
+
 	router := httprouter.New()
 	router.POST("/todos", handler.HomePage)
 	router.GET("/pin", handler.Pin)
