ac5307809c4b8bc29affd2ce61d79b2779b87609
Mori Keigo <yuzumori.keigo@gmail.com>
2020-08-22 22:43:57 +0900

    /pin にアクセスしたら'Welcome to the HomePage!'が返ってくるサーバを作成

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+//w -> 書き出すため
+//r -> ユーザからのリクエスト全て
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Printf("%+v", r)
+	fmt.Println("Endpoint Hit: homePage")
+}
 
 func main() {
-	fmt.Println("Hello GO!!")
+	http.HandleFunc("/pin", homePage)
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
