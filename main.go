aae3b3931844bf54033dc094fdeb9e162aa25781
Mori Keigo <yuzumori.keigo@gmail.com>
2020-08-22 23:21:35 +0900

    JSONをサーバで受け取ってエラーハンドリング

--- main.go
+++ main.go
@@ -1,18 +1,47 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
 
+//メモ
 //w -> 書き出すため
 //r -> ユーザからのリクエスト全て
+//GOはバイナリでbodyが送られるのでJSONにエンコードする必要がある。
+
+//受け取ったときの箱
+type ReqBody struct {
+	Name  string `json:"name"`
+	Age   int    `json:"age"`
+	Todos []Todo `json:"todos"`
+}
+
+type Todo struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
+	body := r.Body
+	defer body.Close()
+	buf := new(bytes.Buffer)                      //受け取り口
+	if _, err := io.Copy(buf, body); err != nil { //bufにbody(r.body)をコピー
+		return
+	}
+	var request ReqBody
+	if err := json.Unmarshal(buf.Bytes(), &request); err != nil { //buf.Bytes() ->(マッピング) 構造体
+		return
+	}
+
 	fmt.Fprintf(w, "Welcome to the HomePage!")
-	fmt.Printf("%+v", r)
+	fmt.Printf("%+v", request)
 	fmt.Println("Endpoint Hit: homePage")
+
 }
 
 func main() {
